8b49a8eef5c01ddac8e2a700ad7b4b84725b504e
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-10-21 17:59:06 +0300

    Add shoebanner

--- src/components/ShoeBanner/ShoeBanner.js
+++ src/components/ShoeBanner/ShoeBanner.js
@@ -1,15 +1,37 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Base = styled.div`
   width: fit-content;
   padding: 8px;
   border-radius: 2px;
   font-family: Raleway;
+  font-size: ${14 / 16}rem;
   font-weight: ${p => p.theme.weights.bold};
+  color: ${p => p.theme.colors.white};
 `;
 
-const ShoeBanner = () => {
-  return <Wrapper>Just released!</Wrapper>;
+const Hot = styled(Base)`
+  background-color: ${p => p.theme.colors.secondary};
+`;
+
+const Sale = styled(Base)`
+  background-color: ${p => p.theme.colors.primary};
+`;
+
+const getComponent = variant => {
+  switch (variant) {
+    case "hot":
+      return Hot;
+    case "sale":
+      return Sale;
+    default:
+      throw new Error(`${variant} was not a proper variant for banner`);
+  }
+};
+
+const ShoeBanner = ({ variant }) => {
+  const Component = getComponent(variant);
+  return <Component>Just released!</Component>;
 };
 
 export default ShoeBanner;
