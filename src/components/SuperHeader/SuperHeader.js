7c3e2411b5e2bef14e77f7420896200fd39960bf
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-10-23 12:00:38 +0300

    polishing

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -3,45 +3,37 @@ import { ShoppingBag } from "react-feather";
 import SearchInput from "../SearchInput";
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 40px;
-  display: flex;
-  justify-content: space-between;
-  padding-left: 32px;
-  padding-right: 32px;
-  font-size: ${14 / 16}rem;
-  background-color: ${p => p.theme.colors.gray900};
   align-items: center;
+  background-color: ${p => p.theme.colors.gray900};
+  display: flex;
+  font-size: ${14 / 16}rem;
+  gap: 24px;
+  height: 40px;
+  padding: 0 32px;
+  width: 100%;
 `;
 
 const AdMessage = styled.p`
   color: ${p => p.theme.colors.white};
-`;
-
-const RightSideWrapper = styled.div`
-  display: flex;
-  gap: 24px;
-  align-items: center;
+  margin-right: auto;
 `;
 
 const Help = styled.p`
   color: ${p => p.theme.colors.gray300};
 `;
 const ShoppingBagIconWrapper = styled(ShoppingBag)`
-  width: 24px;
+  color: ${p => p.theme.colors.gray300};
   height: 24px;
   stroke-width: 1px;
-  color: ${p => p.theme.colors.gray300};
+  width: 24px;
 `;
 
 const SuperHeader = () => (
   <Wrapper>
     <AdMessage>Free shipping on domestic orders over $75!</AdMessage>
-    <RightSideWrapper>
-      <SearchInput />
-      <Help>Help</Help>
-      <ShoppingBagIconWrapper />
-    </RightSideWrapper>
+    <SearchInput />
+    <Help>Help</Help>
+    <ShoppingBagIconWrapper />
   </Wrapper>
 );
 
